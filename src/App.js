746099a3b7531cefcd9a3cd36274ccba755c77f0
Jo prakash Movva <prakash@Jos-MacBook-Pro.local>
2023-02-22 09:21:16 +0530

    i

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Sidebar from './component/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+     <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
